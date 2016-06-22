48d8fc88c4af0d8473f969f8d5702e890ab00a43
sasha42 <sasha42@users.noreply.github.com>
2016-06-22 18:27:18 +0200

    Initial commit

---
+++ gifenc.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+
+# Help text
+usage="$(basename "$0") -w -f -i -o -s -h -- Convert videos to GIFs
+
+where:
+    -w width
+    -f framerate
+    -i input file (required)
+    -o output file
+    -s target size
+    -h help"
+
+# Size calc function
+function videosize {
+    duration=$(ffmpeg -i $input 2>&1 | grep "Duration"| cut -d ' ' -f 4 | sed s/,// | sed 's@\..*@@g' | awk '{ split($1, A, ":"); split(A[3], B, "."); print 3600*A[1] + 60*A[2] + B[1] }')
+    bitrate=$((filesize / duration))
+    echo $bitrate
+}
+
+# Defining defaults
+fps=15
+scale=200
+output=converted_video.gif
+
+# Options tree
+while getopts ':h:w:f:i:o:s:' option; do
+  case "$option" in
+    h) echo "$usage"
+       exit
+       ;;
+    w) scale=$OPTARG
+       ;;
+    f) fps=$OPTARG
+       ;;
+    i) input=$OPTARG
+        filename=$(basename "$input")
+        output="${filename%.*}.gif"
+
+       ;;
+    o) output=$OPTARG
+       ;;
+    s) filesize=$OPTARG
+       targetsize=$(videosize $filesize)
+       echo $targetsize
+       ;;
+    :) printf "missing argument for -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+   \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+# Conversion function
+function convert {
+    palette="/tmp/palette.png"
+
+    filters="fps=10,scale=$scale:-1:flags=lanczos"
+
+    ffmpeg -v warning -i $input -vf "$filters,palettegen" -y $palette
+
+    if [ -n "$targetsize" ]; then
+        echo "\n with bitrate"
+        ffmpeg -v warning -i $input -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -b:v 125k -pass 1 -y -f mp4 /dev/null && \
+        ffmpeg -v warning -i $input -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -b:v 125k -pass 2 -y $output
+    else
+        ffmpeg -v warning -i $input -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $output
+    fi
+}
+
+if [ -n "$input" ]; then
+    convert
+else
+    echo "No input file"
+fi
